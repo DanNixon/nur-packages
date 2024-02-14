3fa4773ad7bbdd99e77ae394e01498c974abf37f
Dan Nixon <dan@dan-nixon.com>
2024-02-14 17:43:55 +0000

    Add dapnetgateway

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   aprsgateway = pkgs.callPackage ./pkgs/aprsgateway { };
+  dapnetgateway = pkgs.callPackage ./pkgs/dapnetgateway { };
   dmrgateway = pkgs.callPackage ./pkgs/dmrgateway { };
   mmdvmhost = pkgs.callPackage ./pkgs/mmdvmhost { };
 }
